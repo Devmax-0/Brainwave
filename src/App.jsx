b192c58d6acb68b5d57c4b782561d464a6d283b2
Devmax_ <muhammadhamzashahid31@gmail.com>
2024-05-02 11:45:58 +0500

    Initial Added

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Benefits from "./Components/Benefits";
+import Collaboration from "./Components/Collaboration";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import Pricing from "./Components/Pricing";
+import Roadmap from "./Components/Roadmap";
+import Services from "./Components/Services";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+export default App;
